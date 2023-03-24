664cb2be3b271b736d205a465e6526c6719e9886
PRANSHU GUPTA <gpranshu482@gmail.com>
2023-03-25 03:01:27 +0530

    first commit

---
+++ src/NewsContext.js
@@ -0,0 +1,25 @@
+import React, { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+export const NewsContext = createContext();
+
+export const NewsContextProvider = (props) => {
+  const [data, setData] = useState();
+  const apiKey = "a268451f984343aca5e6f2d33a60e747";
+
+  
+  useEffect(() => {
+    axios
+      .get(
+        `https://newsapi.org/v2/everything?q=keyword&apiKey=${apiKey}`
+      )
+      .then((response) => setData(response.data))
+      .catch((error) => console.log(error));
+  }, []);
+
+  return (
+    <NewsContext.Provider value={{ data }}>
+      {props.children}
+    </NewsContext.Provider>
+  );
+};
